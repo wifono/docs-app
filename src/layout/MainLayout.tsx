ac3e9d7070e3f0fe4e7d4f7d93b2c4c55ca18c2c
Daniel Tršťanský <daniel.trstansky@gmail.com>
2025-11-27 08:51:57 +0100

    add: Main layout

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react"
+import Header from "../components/HeaderComponent"
+
+type MainLayoutProps = {
+  children: ReactNode
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 p-4">
+        {children}
+      </main>
+    </div>
+  )
+}
