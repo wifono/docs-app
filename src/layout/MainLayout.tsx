7a0119133e39c79ea29bfcb71701cbc56cf430bf
Daniel Tršťanský <daniel.trstansky@gmail.com>
2025-11-27 13:18:12 +0100

    remove: padding

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -9,7 +9,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 p-4">
+      <main className="flex-1">
         {children}
       </main>
     </div>
