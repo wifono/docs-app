cb03e402f1f2af51555d5f7f3917f976a4e43a1d
Daniel Tršťanský <daniel.trstansky@gmail.com>
2025-11-29 18:46:40 +0100

    add: apply auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import AppRoutes from './app/AppRoutes'
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Toaster position="top-center" />
       <AppRoutes />
-    </>
+    </AuthProvider>
   )
 }
 
