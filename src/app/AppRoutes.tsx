35d97f07a61308acfc2c0933e47d17b56e2694be
Daniel Tršťanský <daniel.trstansky@gmail.com>
2025-12-01 09:08:19 +0100

    add: documents route

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import MainLayout from "../layout/MainLayout"
 
 import Home from "../pages/Home"
+import Documents from "../pages/Documents"
 
 export default function AppRoutes() {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/documents" element={<Documents />} />
       </Routes>
     </MainLayout>
   )
